6878f790a158791339e455bdf9b7f6a62cdbc2f5
MMARTID <miguelmartindelgado1998@gmail.com>
2025-01-07 14:51:15 +0100

    Day 1

--- server/app.js
+++ server/app.js
@@ -1,33 +1,50 @@
+require('dotenv').config();
 const express = require("express");
 const morgan = require("morgan");
-const cookieParser = require("cookie-parser");
-const PORT = 5005;
+const PORT = process.env.PORT;
 
+const app = express(); // inicializa express ;)
 // STATIC DATA
+const cors = require("cors");
+const cohorts = require("./cohorts.json")
+const students = require("./students.json");
 // Devs Team - Import the provided files with JSON data of students and cohorts here:
-// ...
+app.get("/api/cohorts", (req, res) => {
+  res.json(cohorts);
+});
 
-
-// INITIALIZE EXPRESS APP - https://expressjs.com/en/4x/api.html#express
-const app = express();
-
-
+app.get("/api/students", (req, res) => {
+  res.json(students);
+});
 // MIDDLEWARE
 // Research Team - Set up CORS middleware here:
 // ...
+app.use(
+  cors({
+    // Add the URLs of allowed origins to this array
+    origin: ['http://localhost:5173'],
+  })
+);
+
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+
 
 
 // ROUTES - https://expressjs.com/en/starter/basic-routing.html
 // Devs Team - Start working on the routes here:
 // ...
+
+
+
+
 app.get("/docs", (req, res) => {
   res.sendFile(__dirname + "/views/docs.html");
 });
+
+
 
 
 // START SERVER
